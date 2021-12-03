7f005214b6b1826dee0f7d2d63cfb837126ba560
Innei <tukon479@gmail.com>
2021-12-03 22:04:37 +0800

    feat: git calendar day

--- src/components/code-tiles/index.tsx
+++ src/components/code-tiles/index.tsx
@@ -1,13 +1,41 @@
+import clsx from 'clsx'
+import { range } from 'lodash-es'
 import React from 'react'
+import styles from './index.module.css'
 
-const size = 14
-const width = 14 * size
-const height = size * (364 / 14)
+const size = 10
+const gap = 5
+const day = 364
+const rowCount = 14
+const width = rowCount * size + (rowCount - 1) * gap
+const height = size * (day / rowCount) + (day / rowCount - 1) * gap
 export const CodeTiles: React.FC = () => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null)
   return (
-    <div className="inline-block">
-      <canvas height={height} width={width} ref={canvasRef}></canvas>
+    <div className={clsx('inline-block', styles['root'])}>
+      <svg height={height} width={width}>
+        <g>
+          {range(0, day).map((i) => {
+            return (
+              <rect
+                key={i}
+                width="11"
+                height="11"
+                x={size * (i % rowCount) + gap * (i % rowCount)}
+                y={
+                  size * Math.floor(i / rowCount) +
+                  gap * Math.floor(i / rowCount)
+                }
+                className="ContributionCalendar-day"
+                rx="2"
+                ry="2"
+                data-count="7"
+                data-date="2020-11-29"
+                data-level={Math.floor(Math.random() * 3)}
+              ></rect>
+            )
+          })}
+        </g>
+      </svg>
     </div>
   )
 }
