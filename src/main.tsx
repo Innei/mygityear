674099aa7d7228534582fbd5118e9652ef53bb41
Innei <tukon479@gmail.com>
2021-12-04 20:35:04 +0800

    feat: p4

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { Background } from './components/background'
-import { Part1 } from './sections/part1'
-import { Part2 } from './sections/part2'
 import './index.css'
-import { Part3 } from './sections/part3'
+const parts = import.meta.glob('./sections/*.tsx')
 
 const App = document.getElementById('root')
 const Root: FC = () => {
+  const [FCGroup, setGroup] = React.useState<FC[]>([])
+  useEffect(() => {
+    ;(async () => {
+      const modules = await Promise.all(
+        Object.values(parts).map((promise) => promise()),
+      )
+      const fc = [] as any
+      modules.forEach((mo) => {
+        fc.push(Object.values(mo)[0])
+      })
+      setGroup(fc)
+    })()
+  }, [])
   return (
     <>
       <Background></Background>
-      <Part1 />
-      <Part2 />
-      <Part3 />
+
+      {FCGroup.map((fc) => React.createElement(fc, { key: fc.name }))}
     </>
   )
 }
---
+++ src/sections/part4.tsx
@@ -0,0 +1,65 @@
+import clsx from 'clsx'
+import React from 'react'
+import styles from './index.module.css'
+
+export const Part4: React.FC = () => {
+  const mostUsedLanguage: string | undefined = 'typescript'
+  const otherUsedLanguages = ['swift', 'java', 'c++']
+  const howManyLine = 162111
+  return (
+    <section className={clsx(styles['part4'], 'font-medium container')}>
+      <div>
+        <p className="text-3xl font-medium">
+          {mostUsedLanguage ?? '我不知道你写了什么'}
+        </p>
+
+        <p className="mt-2">
+          {mostUsedLanguage
+            ? '这是我最常用的语言。'
+            : '应该是太冷门了吧，数据库里找不到对应的语言。🥲'}
+        </p>
+
+        <div className="gap"></div>
+
+        <p>
+          <span>
+            在这一年里，我使用这门语言提交了{' '}
+            <span className="text-3xl text-yellow-400">
+              {howManyLine.toLocaleString()}{' '}
+            </span>
+            行代码。
+          </span>
+
+          <br />
+          <span>他是我最好的伙伴。</span>
+        </p>
+
+        <div className="gap"></div>
+        {otherUsedLanguages.length > 0 ? (
+          <div className="leading-7">
+            <p>在剩余的时光里，</p>
+            <p>
+              {otherUsedLanguages.map((l) => (
+                <span className="text-3xl text-purple-400" key={l}>
+                  {l}{' '}
+                </span>
+              ))}
+            </p>
+            <p>他们也陪我走过一段旅程。</p>
+          </div>
+        ) : (
+          <div>
+            <p>我很专一，没有使用过其他的语言。</p>
+          </div>
+        )}
+
+        {otherUsedLanguages.length > 6 && (
+          <>
+            <div className="gap"></div>
+            <p>语言大师的称号，非你莫属！</p>
+          </>
+        )}
+      </div>
+    </section>
+  )
+}
